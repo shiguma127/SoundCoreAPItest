e6c76d71a544ff519ff918f71ab18ac2afa4a0bf
shiguma127 <shiguma127@gmail.com>
2021-10-25 01:11:31 +0900

    プロジェクト ファイルを追加します。

---
+++ SoundCoreAPItest/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Threading;
+using CSCore.CoreAudioAPI;
+
+namespace SoundCoreAPItest
+{
+    class Program
+    {
+        [MTAThread]
+
+        static void Main(string[] args)
+        {
+            VolumeManager manager = new VolumeManager();
+            SerialPort serialPort = new SerialPort("COM3");
+            serialPort.BaudRate = 9600;
+            serialPort.Parity = Parity.None;
+            serialPort.StopBits = StopBits.One;
+            serialPort.DataBits = 8;
+            serialPort.Handshake = Handshake.None;
+            serialPort.RtsEnable = true;
+            serialPort.DataReceived += new SerialDataReceivedEventHandler((o,e) => DataReceivedHandler(o,e,manager));
+            serialPort.Open();
+            Thread.CurrentThread.Join();
+        }
+        private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e, VolumeManager instance)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadLine().Trim();
+            try
+            {
+                indata = indata.Split(':')[1];
+                float volume = (float)(Double.Parse(indata) * 0.01);
+                instance.SetProcessVolume("Spotify", volume);
+            }
+            catch
+            {
+                //ignore
+            }
+        }
+
+
+        private static AudioSessionManager2 GetDefaultAudioSessionManager(DataFlow dataFlow)
+        {
+            using (var enumerator = new MMDeviceEnumerator())
+            {
+                using (var device = enumerator.GetDefaultAudioEndpoint(dataFlow, Role.Multimedia))
+                {
+                    using (AudioEndpointVolume endpointVolume = AudioEndpointVolume.FromDevice(device))
+                    {
+                        //デバイスの音量を取得
+                        Debug.WriteLine("DefaultDevice: " + device.FriendlyName);
+                        Debug.WriteLine("Volume: " + endpointVolume.MasterVolumeLevel);
+                        AudioSessionManager2 sessionManager = AudioSessionManager2.FromMMDevice(device);
+                        return sessionManager;
+                    }
+                }
+            }
+        }
+
+        
+
+        private static void SetMasterVolume(float volume)
+        {
+            using (var enumerator = new MMDeviceEnumerator())
+            {
+                using (var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia))
+                {
+                    using (AudioEndpointVolume endpointVolume = AudioEndpointVolume.FromDevice(device))
+                    {
+                            endpointVolume.MasterVolumeLevelScalar = volume;
+                    }
+                }
+            }
+        }
+        
+    }
+
+    
+}
---
+++ SoundCoreAPItest/VolumeManager.cs
@@ -0,0 +1,92 @@
+﻿using CSCore.CoreAudioAPI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SoundCoreAPItest
+{
+    class VolumeManager
+    {
+        public List<(SimpleAudioVolume, AudioSessionControl2)> list;
+
+        public VolumeManager()
+        {
+            list = new List<(SimpleAudioVolume, AudioSessionControl2)>();
+            using (var sessionManager = GetDefaultAudioSessionManager(DataFlow.Render))
+            {
+                AudioSessionEnumerator sessionEnumerator = sessionManager.GetSessionEnumerator();
+                foreach (var session in sessionEnumerator)
+                {
+                    try
+                    {
+                        // usingで書くべき箇所
+
+                        var simpleVolume = session.QueryInterface<SimpleAudioVolume>();
+                        var sessionControl = session.QueryInterface<AudioSessionControl2>();
+                        Process process = Process.GetProcessById(sessionControl.ProcessID);
+                        Console.Write(process.ProcessName + "[" + sessionControl.ProcessID + "]");
+                        //Console.WriteLine(process.MainModule.FileName);
+                        //システムのメインモジュールは取得できないのでid[0]で弾く
+                        //filepathからiconを取得できる
+                        //Icon ico = Icon.ExtractAssociatedIcon(@"C:\WINDOWS\system32\notepad.exe");
+                        Console.Write(" : ");
+                        Console.WriteLine(simpleVolume.MasterVolume);
+                        //session.RegisterAudioSessionNotification(events);
+                        list.Add((simpleVolume, sessionControl));
+                    }
+                    catch (ArgumentException)
+                    {
+                        //エラーを握りつぶす
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+            list.ForEach((item) =>
+            {
+                /*
+                 * Processを取りに行くのが遅いのでクラス作ってProcess名とSimpleAudioVolumeを取りに行くのが良いかも
+                 * イベントのためにAudioSessionControlも必要
+                 *
+                */
+                
+                item.Item2.SimpleVolumeChanged += (o,e)=> { Console.WriteLine(item.Item2.Process.ProcessName + ":" + e.NewVolume); };
+            });
+        }
+
+
+        public void SetProcessVolume(String processName, float volume)
+        {
+            foreach (var item in list)
+            {
+                if (item.Item2.Process.ProcessName.Equals(processName))
+                {
+                    item.Item1.MasterVolume = volume;
+                }
+            }
+        }
+
+
+
+
+        private static AudioSessionManager2 GetDefaultAudioSessionManager(DataFlow dataFlow)
+        {
+            using (var enumerator = new MMDeviceEnumerator())
+            {
+                using (var device = enumerator.GetDefaultAudioEndpoint(dataFlow, Role.Multimedia))
+                {
+                    using (AudioEndpointVolume endpointVolume = AudioEndpointVolume.FromDevice(device))
+                    {
+                        //デバイスの音量を取得
+                        Debug.WriteLine("DefaultDevice: " + device.FriendlyName);
+                        Debug.WriteLine("Volume: " + endpointVolume.MasterVolumeLevel);
+                        AudioSessionManager2 sessionManager = AudioSessionManager2.FromMMDevice(device);
+                        return sessionManager;
+                    }
+                }
+            }
+        }
+    }
+}
